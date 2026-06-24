c61018d962898fc8ab43625e2183b5618f1b0132
k-beee <296437372+k-beee@users.noreply.github.com>
2026-06-24 18:07:32 +0100

    Support dynamic server-less build on Vercel by making static HTML export conditional

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  ...(process.env.NEXT_PUBLIC_EXPORT ? { output: "export" } : {}),
   images: { unoptimized: true },
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
   trailingSlash: true,
